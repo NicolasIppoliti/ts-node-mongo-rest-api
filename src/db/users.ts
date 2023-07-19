4c84e87587946b203a00a2374737727affa72549
Nicolas Ippoliti <contacto.nicolasippoliti@gmail.com>
2023-07-19 17:14:53 -0300

    API-REST v1.0.0

---
+++ src/db/users.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, unique: true, required: true },
+  username: { type: String, unique: true, required: true },
+  authentication: {
+    password: { type: String, required: true, select: false },
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false },
+  } as Record<string, any>,
+  role: { type: String, enum: ["admin", "premium", "user"], default: "user" },
+});
+
+export const UserModel = mongoose.model("User", UserSchema);
+
+// User Actions
+export const getUsers = () => UserModel.find();
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({ 'authentication.sessionToken': sessionToken });
+export const getUserById = (id: string) => UserModel.findById(id);
+export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject());
+export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
